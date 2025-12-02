e24104a03ee44f24e54c5987aae7ae08d92398eb
Yap Zhi Qian <113446571+zhiqian21@users.noreply.github.com>
2025-12-02 13:13:04 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cjb6ch5mhfa6.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "zq-s3-bucket"
+customregion = "us-east-1"
